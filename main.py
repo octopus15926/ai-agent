ed20abf60106b8d598da98ed8f029321f3a62211
dylan <186506596+octopus15926@users.noreply.github.com>
2025-07-21 17:53:09 -0400

    Implement prompting

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
+    if len(sys.argv) >= 2:
+        user_prompt = sys.argv[1]
+        if user_prompt != '':
+            messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)])
+            ]
+            response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+            print(response.text)
+        else:
+            raise Exception("Did you forget to include a question?")
+            return -1
+        if "--verbose" in sys.argv:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
