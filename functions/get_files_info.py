3e5b4defd9777142c8050ffb999a73f4f1ec449b
dylan <186506596+octopus15926@users.noreply.github.com>
2025-07-22 19:39:12 -0400

    Implement get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,36 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    file_path = ''
+    dir_path = os.path.join(working_directory, directory)
+    directory_string = directory
+    if directory == '.':
+        directory_string = "current"
+    result_string = ""
+
+    if not os.path.isdir(dir_path):
+        result_string += f'Error: "{directory}" is not a directory'
+        return result_string
+
+    if not os.path.abspath(dir_path).startswith(os.path.abspath(working_directory)):
+        result_string += f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return result_string
+        
+    contents = None
     try:
-        file_path = os.path.join(working_directory, directory)
-    except:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
-
+        contents = os.listdir(dir_path)
+    except Exception:
+        result_string += f"Error: Invalid directory path"
+        return result_string
     
+    result_string = f"Result for {directory_string} directory:\n"
+    if contents:
+        for file in contents:
+            file_path =  f"{dir_path}/{file}"
+            try:
+                file_size = os.path.getsize(file_path)
+                file_is_dir = os.path.isdir(file_path)
+            except:
+                result_string += f"Error: Invalid file path"
+                return result_string
+            result_string += f"- {file}: file_size={file_size}, is_dir={file_is_dir}\n"
+    return result_string
